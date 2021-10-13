0db82f6cc8bd3a38fefe05bb93799b32f405b80a
MarcoEsquivelBasaldua <marco.esquivel@cimat.mx>
2021-10-13 00:35:05 -0500

    simple pendulum

--- Simple pendulum/SimplePendulum.m
+++ Simple pendulum/SimplePendulum.m
@@ -2,7 +2,7 @@
 param.g = 9.8;  % gravity
 param.l = 1;    % pendulum length
 param.m = 1;    % pendulum mass
-param.k = 0.25; % damp ratio
+param.b = 0.25; % damp ratio
 
 % Initial position q0 = [q dq]
 q0 = [pi/8 0];
@@ -20,5 +20,5 @@ function dQ = Pendulum(t, Q, param)
     % States output dQ = [dq ddq]
     dQ = zeros(2,1);
     dQ(1) = Q(2);
-    dQ(2) = -param.g/param.l*sin(Q(1)) - (param.k*Q(2))/param.m;
+    dQ(2) = -param.g/param.l*sin(Q(1)) - (param.b*Q(2))/param.m;
 end
